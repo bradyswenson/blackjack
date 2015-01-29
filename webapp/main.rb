7cda17babc7c0289febbf82f431e3bedaf0c0c40
Brady Swenson <bradyswenson@gmail.com>
2015-01-29 11:13:49 -0600

    post baby restart :)

--- webapp/main.rb
+++ webapp/main.rb
@@ -1,8 +1,25 @@
 require 'rubygems'
 require 'sinatra'
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'dhsndumw7263ns6y2mskd92wsh' 
 
+
+get '/' do
+  unless session[:player_name]
+    redirect '/new_player'
+  end
+  erb :index
+end
+
+get '/new_player' do
+  erb :get_player_name
+end
+
+post '/new_player' do
+  session[:player_name] = params['username']
+end
 
 
 
